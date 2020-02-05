7fc098ce086ae46ae22cdc243c08cd8842619f4f
Darrrah <darashalabi@gmail.com>
2020-02-05 15:29:05 +0200

    lab06 done

--- lab06.js
+++ lab06.js
@@ -1,1 +1,8 @@
- alert (this is an alert box)
+var name=prompt("What's your name?");
+alert("Nice to meet you!");
+var day=confirm ("How is your day going?");
+if (day==true) {
+    document.write ("Hope your day keeps going good!");
+}
+else {document.write ("Hope your day gets better!");
+}
