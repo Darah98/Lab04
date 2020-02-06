cda03076cc9d4fb7c18a92e4bc3e3222ca31e200
Darrrah <darashalabi@gmail.com>
2020-02-06 03:17:28 +0200

    edited lab06

--- lab06.js
+++ lab06.js
@@ -1,8 +1,26 @@
-var name=prompt("What's your name?");
-alert("Nice to meet you!");
-var day=confirm ("How is your day going?");
-if (day==true) {
-    document.write ("Hope your day keeps going good!");
-}
-else {document.write ("Hope your day gets better!");
-}
+// function(welcomeVisitor) {
+    //var name=prompt("What's your name?");
+//alert("Nice to meet you!");
+//var day=confirm ("How is your day going?");
+//if (day==true) {
+  //  document.write ("Hope your day keeps going good!");
+//}
+//else {document.write ("Hope your day gets better!");
+//} };
+
+function bmiCalculator() {
+    var x = prompt(“Input Weight in kilograms”);
+     var y = prompt(“Input Height in meters”); 
+parseInt(String: Number)
+var weight= parseInt(x);
+var height = parseInt(y);
+     var bmi = weight/height; 
+     if (bmi >= 25) { 
+         alert=(bmi , “Overweight”); 
+       } else if (bmi >= 18.5 && bmi <= 24.9) {
+           alert=(bmi , "Normal"); 
+        } else  {
+               alert=(bmi , "Underweight"); 
+               return bmi;
+        }
+   function bmiCalculator();
